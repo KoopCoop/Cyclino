822607d3fa3bb10d23b22fbb0de7bd82d863b02d
KoopCoop <xxengineering@outlook.de>
2018-03-31 17:07:22 +0200

    Aufruf bytestohex entfernt

--- app/src/main/java/biz/endotherm/NFC/HandleTag.java
+++ app/src/main/java/biz/endotherm/NFC/HandleTag.java
@@ -725,7 +725,7 @@ public class HandleTag {
     }
 
     //parsing function
-    final private static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    /*final private static char[] hexArray = "0123456789ABCDEF".toCharArray();
     private static String bytesToHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 3];
         for ( int j = 1; j < bytes.length; j++ ) {
@@ -733,6 +733,16 @@ public class HandleTag {
             hexChars[j * 3] = hexArray[v >>> 4];
             hexChars[j * 3 + 1] = hexArray[v & 0x0F];
             hexChars[j * 3 + 2] = ' ';
+        }
+        return new String(hexChars);
+    }*/
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
         }
         return new String(hexChars);
     }
